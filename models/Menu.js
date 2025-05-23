2d5b1a5652905a7ed0a43ea5570c8393007911fe
thighburger <lewis177777@gmail.com>
2025-05-23 22:54:36 +0900

    가게,메뉴api테스트

--- models/Menu.js
+++ models/Menu.js
@@ -1,14 +1,41 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+// const menuSchema = new Schema({
+//     name: { type: String, required: true },
+//     price: { type: Number },
+//     image: { type: String },
+//     description: { type: String },
+//     reviews: [{ type: Schema.Types.ObjectId, ref: 'Review' }],
+//     likeCount: { type: Number, default: 0 },
+//     likedUsers: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+// });
+
 const menuSchema = new Schema({
-    name: { type: String, required: true },
-    price: { type: Number },
-    image: { type: String },
-    description: { type: String },
-    reviews: [{ type: Schema.Types.ObjectId, ref: 'Review' }],
-    likeCount: { type: Number, default: 0 },
-    likedUsers: [{ type: Schema.Types.ObjectId, ref: 'User' }]
-});
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  reviews: {
+    type: [String],
+    default: []
+  },
+  likeCount: {
+    type: Number,
+    default: 0
+  },
+  heart: {
+    type: Boolean,
+    default: false
+  }
+},);
 
 module.exports = mongoose.model('Menu', menuSchema);
--- models/Store.js
+++ models/Store.js
@@ -2,11 +2,42 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+// const storeSchema = new Schema({
+//     name: { type: String, required: true },
+//     location: { type: String, required: true },
+//     menus: [{ type: Schema.Types.ObjectId, ref: 'Menu' }], // 배열로 수정
+//     image: { type: String }
+// });
+
 const storeSchema = new Schema({
-    name: { type: String, required: true },
-    location: { type: String, required: true },
-    menus: [{ type: Schema.Types.ObjectId, ref: 'Menu' }], // 배열로 수정
-    image: { type: String }
-});
+    name: {
+      type: String,
+      required: true
+    },
+    location: {
+      type: String,
+      required: true
+    },
+    menus: {
+      type: [String],
+      default: []
+    },
+    image: {
+      type: String,
+      required: true
+    },
+    likeSum: {
+      type: Number,
+      default: 0
+    },
+    locationCategory: {
+      type: String,
+      required: true
+    },
+    foodCategory: {
+      type: String,
+      required: true
+    }
+  });
 
 module.exports = mongoose.model('Store', storeSchema);
