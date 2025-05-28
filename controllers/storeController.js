e3b46c5e74ce96439dff3dc4f8a23534c4ef9948
thighnurger <lewis177777gmail.com>
2025-05-28 20:26:15 +0900

    오타수정?

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -8,6 +8,7 @@ const getAllStores = async (req, res) => {
     //likeSum을 기준으로 내림차순 정렬
     stores.sort((a, b) => b.likeSum - a.likeSum);
     res.json(stores);
+  
   } catch (err) {
     res.status(500).json({ error: '가게 데이터를 가져오는 데 실패했습니다.' });
   }
@@ -25,16 +26,16 @@ const getMenusByStore = async (req, res) => {
     }
 
     const menuIds = store.menus.map(menu => menu._id); // 가게의 메뉴 ID 목록
-    console.log('store.menus:', menuIds); // store.menus 배열 출력
+ 
 
     const likedMenuIds = await Like.find({
       userId: userId,
       menuId: { $in: menuIds } // $in 연산자를 사용하여 menuIds 배열과 일치하는 메뉴를 찾음
-    }).then(likes => likes.map(like => like.menuId));
+    }).then(likes => likes.map(like => like.menuId). toString()); // 좋아요한 메뉴 ID 배열 생성
 
     const menu = store.menus;
-    console.log(likedMenuIds)
-
+    //console.log('store.menus:', store.menus); // 가게의 메뉴 배열 출력
+    //console.log('likedMenuIds:', likedMenuIds); // 좋아요한 메뉴 ID 배열 출력
     const menusWithLiked = menu.map(menuItem => {
       let isLiked = likedMenuIds.includes(menuItem._id); // 좋아요 여부 확인
       return {
@@ -42,7 +43,7 @@ const getMenusByStore = async (req, res) => {
         heart: isLiked, // 좋아요 여부 추가
       };
     });
-    console.log('menusWithLiked:', menusWithLiked); // 좋아요 여부가 추가된 메뉴 배열 출력
+    //console.log('menusWithLiked:', menusWithLiked); // 좋아요 여부가 추가된 메뉴 배열 출력
     //메뉴 좋아요 기준으로 내림차순 정렬
     menusWithLiked.sort((a, b) => b.likeCount - a.likeCount);
     res.json(menusWithLiked);
