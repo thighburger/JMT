8f6c555bcf23c80d02c56aedaf66b50c25fcbbe7
thighburger <lewis177777@gmail.com>
2025-07-18 20:03:31 +0900

    가게추천 인기1위메뉴 함께 반환 popularMenu

--- controllers/recommendController.js
+++ controllers/recommendController.js
@@ -1,11 +1,12 @@
 const Store = require('../models/Store');
+const Menu = require('../models/Menu'); // Menu 모델을 상단에 추가
 
 const getRecommendStore = async (req, res) => {
     try {
         const locationCategory = req.params.locationCategory; // URL 파라미터에서 locationCategory를 가져옵니다.
 
-        // 1. 해당 locationCategory에 맞는 가게들을 모두 조회합니다.
-        const stores = await Store.find({ locationCategory: locationCategory });
+        // 1. 해당 locationCategory에 맞는 가게들을 모두 조회합니다. (menus 필드 제외)
+        const stores = await Store.find({ locationCategory: locationCategory }).select('-menus');
 
         // 2. 조회된 가게가 없는 경우 처리합니다.
         if (stores.length === 0) {
@@ -16,8 +17,18 @@ const getRecommendStore = async (req, res) => {
         const randomIndex = Math.floor(Math.random() * stores.length);
         const recommendStore = stores[randomIndex];
 
-        // 4. 선택된 가게를 응답으로 보냅니다.
-        res.status(200).json(recommendStore);
+        // 4. 선택된 가게에서 좋아요가 가장 많은 메뉴의 이름을 조회합니다.
+        const topMenu = await Menu.findOne({ storeId: recommendStore._id })
+            .sort({ likeCount: -1 })
+            .select('name');
+
+        // 5. 가게 객체에 popularMenu 필드를 추가하여 응답으로 보냅니다.
+        const storeWithPopularMenu = {
+            ...recommendStore.toObject(),
+            popularMenu: topMenu ? topMenu.name : null
+        };
+
+        res.status(200).json(storeWithPopularMenu);
 
     } catch (err) {
         console.error('가게 추천 조회 중 오류 발생:', err); // 에러 로그를 좀 더 상세하게
--- routes/recommendRoutes.js
+++ routes/recommendRoutes.js
@@ -7,8 +7,8 @@ const {getRecommendStore}=require('../controllers/recommendController');
  * @swagger
  * /recommend/{locationCategory}:
  *   get:
- *     summary: 가게 랜덤 반환
- *     description: 상대,후문,정문 받아서 그중 가게 랜덤반환.
+ *     summary: 가게 랜덤 반환 (인기 메뉴명 포함)
+ *     description: 상대,후문,정문 받아서 그중 가게 랜덤반환하고, 해당 가게의 가장 좋아요가 많은 메뉴명을 popularMenu 필드로 포함하여 반환. menus 배열은 제외됨.
  *     tags: [Recommend]
  *     parameters:
  *       - in: path
@@ -16,16 +16,38 @@ const {getRecommendStore}=require('../controllers/recommendController');
  *         required: true
  *         schema:
  *           type: string
+ *           enum: [상대, 후문, 정문]
  *         description: 위치분류
  *     responses:
  *       200:
- *         description: 가게 반환 성공
+ *         description: 가게 및 인기 메뉴명 반환 성공 (menus 배열 제외)
  *         content:
  *           application/json:
  *             schema:
- *               type: array
- *               items:
- *                 $ref: '#/components/schemas/Store'
+ *               allOf:
+ *                 - $ref: '#/components/schemas/Store'
+ *                 - type: object
+ *                   properties:
+ *                     popularMenu:
+ *                       type: string
+ *                       nullable: true
+ *                       description: 해당 가게의 가장 좋아요가 많은 메뉴명
+ *             example:
+ *               isAllowed: false
+ *               _id: "68616eb883c963aea392e6d1"
+ *               name: "전대맛칼국수"
+ *               location: "광주 북구 반룡로42번길 26 1층"
+ *               image: "https://jmt-bucket-01.s3.ap-northeast-2.amazonaws.com/전대맛칼국수 전남대점/대표사진.jpg"
+ *               likeSum: 0
+ *               locationCategory: "상대"
+ *               foodCategory: "한식"
+ *               __v: 1
+ *               displayedImg: null
+ *               popularMenu: "바지락칼국수"
+ *       404:
+ *         description: 해당 지역 카테고리에 맞는 가게가 없습니다
+ *       500:
+ *         description: 서버 오류
  */
 router.get('/:locationCategory', getRecommendStore); 
 
